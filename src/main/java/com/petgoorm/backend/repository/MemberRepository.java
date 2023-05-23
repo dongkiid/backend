613510d37668f4ee614d76cef562c1722231dd7a
Baekys97 <ys0907kr@naver.com>
2023-05-24 08:55:29 +0900

    (feat: login service 기능 추가)

--- src/main/java/com/petgoorm/backend/repository/MemberRepository.java
+++ src/main/java/com/petgoorm/backend/repository/MemberRepository.java
@@ -4,9 +4,12 @@ import com.petgoorm.backend.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
 
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
